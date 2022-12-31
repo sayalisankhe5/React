368ff46f5579e0ded999f355e9b56d7bd09f6d6d
sayalisankhe5 <sayalisankhe96@gmail.com>
2022-12-31 15:57:19 +0530

    bundlers

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   {
     id: "ch1",
     className: "chn1",
   },
-  "Namaste React from SS"
+  "Namaste React from Sayali S"
 );
 const heading2 = React.createElement("h2", {}, "Heading2");
-const container = React.createElement("div", {}, [heading, heading2]);
+const container = React.createElement("div", { some: "value" }, [
+  heading,
+  heading2,
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(heading);
 root.render([container, heading2]);
