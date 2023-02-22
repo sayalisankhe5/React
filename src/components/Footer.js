0a52801a92d8746a45b21554925a06d43c82ec01
sayalisankhe5 <86399171+sayalisankhe5@users.noreply.github.com>
2023-02-22 10:14:20 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <h4>footer</h4>;
+  return (
+    <div className="w-full  h-10 bg-black">
+      <div className=" flex justify-center align-middle font-bold  text-white">
+        <span>&#x24B8; </span> Sayali Sankhe
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
